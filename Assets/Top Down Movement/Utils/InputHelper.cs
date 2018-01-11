77adb52c93603996260074bcf21c8c3e5eccfaca
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2018-01-11 17:27:20 -0300

    Dumbest movement Scene

---
+++ Assets/Top Down Movement/Utils/InputHelper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace AntonioHR.MovementExamples
+{
+    public static class InputHelper
+    {
+        public static Vector2 DefaultJoystickInput { get { return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); } }
+
+        public static Vector2 DefaultJoystickInputRaw { get { return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); } }
+    }
+}
