5401e0b688d9298b602d37a919204b6b9c893476
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2018-01-12 16:19:02 -0300

    Rigidbody movement Prototype

---
+++ Assets/Top Down Movement/02/RigibodyMoveChar.cs
@@ -0,0 +1,34 @@
+﻿using AntonioHR.MovementExamples;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RigibodyMoveChar : MonoBehaviour {
+    Rigidbody2D rb;
+
+    public float moveForce = 5.0f;
+    public bool doBrake = false;
+
+    public BrakeSettings brakeSettings;
+
+    public class BrakeSettings
+    {
+        public float stoppingThreshold = 0.001f;
+        public float breakForceMag = 4.0f;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(InputHelper.DefaultJoystickInputRaw * moveForce, ForceMode2D.Force);
+
+        if (doBrake)
+        {
+            //rb.AddForce()
+        }
+    }
+}
