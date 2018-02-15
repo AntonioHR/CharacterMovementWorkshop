747097126c7ec443775e1810f02c6444ddbc5b87
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2018-02-15 11:37:36 -0300

    Top down Movement with new Architecture

---
+++ Assets/3_FSM Character Controller/Code/AntonioHR/FSMMoveExamples/Inputs/IPlayerInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace AntonioHR.FSMMoveExamples.Inputs
+{
+    public interface IPlayerInput
+    {
+        Vector2 Axes { get; }
+
+        void Update();
+    }
+}
---
+++ Assets/3_FSM Character Controller/Code/AntonioHR/FSMMoveExamples/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿
+using AntonioHR.FSMMoveExamples.Movements;
+
+namespace AntonioHR.FSMMoveExamples
+{
+    public class PlayerMovement
+    {
+
+        private IAxisMovement xAxisMode;
+        private IAxisMovement yAxisMode;
+        private Player owner;
+
+
+        public PlayerMovement(Player owner)
+        {
+            this.owner = owner;
+        }
+
+
+        public IAxisMovement XAxisMode
+        {
+            get { return xAxisMode; }
+            set
+            {
+                xAxisMode = value;
+                xAxisMode.Setup(Axis2.X, owner.Body);
+            }
+        }
+        public IAxisMovement YAxisMode
+        {
+            get { return yAxisMode; }
+            set
+            {
+                yAxisMode = value;
+                yAxisMode.Setup(Axis2.Y, owner.Body);
+            }
+        }
+
+
+
+        public void Update()
+        {
+            xAxisMode.Update();
+            yAxisMode.Update();
+        }
+
+        public void Simulate()
+        {
+            xAxisMode.Simulate();
+            yAxisMode.Simulate();
+        }
+    }
+}
